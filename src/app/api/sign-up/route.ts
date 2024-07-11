3666a2b3540f38395be86a86a48fc1915b4305c8
Abhigyan <singhdefinite12@gmail.com>
2024-07-11 16:45:48 +0530

    routes changes

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -25,22 +25,68 @@ export async function POST (request: Request){
 
         const existingUserByEmail = await UserModel.findOne({email})
 
+        const verifyCode = Math.floor(100000 + Math.random()*90000 ).toString()
+
         if (existingUserByEmail){
-            true // todo back here 
+            if (existingUserByEmail.isverified){
+                return Response.json(
+                    {
+                      success: false,
+                      message:"email already exist with this user",
+                    },
+                    { status: 400 }
+                  );
+             }
+            else{
+                const hashedPassword = bcrypt.hash(password,10)
+                    existingUserByEmail.password = hashedPassword;
+                    existingUserByEmail.verifycode = verifyCode;
+                    existingUserByEmail.verifycodeExpiry = new Date(Date.now()+ 3600000);
+                    await existingUserByEmail.save()
+
+                }
         }
         else {
            const hashedPassword =  await bcrypt.hash(password,10)
             const expiryDate = new Date()
            expiryDate.setHours(expiryDate.getHours()+1)
-        }
+      
 
-        new UserModel({
+       const newUser =  new UserModel({
             username,
             email,
-         
-
-        })
-        
+            password : hashedPassword,
+            verifyCode,
+            verifycodeExpiry:expiryDate,
+            isverified : false ,
+            isAccepting :true,
+            messages :[],
+ });
+ await newUser.save();
+    }
+      // Send verification email
+      const emailResponse = await sendverificationEmail(
+        email,
+        username,
+        verifyCode
+      );
+      if (!emailResponse.success) {
+        return Response.json(
+          {
+            success: false,
+            message: emailResponse.message,
+          },
+          { status: 500 }
+        );
+      }
+  
+      return Response.json(
+        {
+          success: true,
+          message: 'User registered successfully. Please verify your account.',
+        },
+        { status: 201 }
+      );
     } catch (error) {
         console.error("error registering user",error)    
         return Response.json({
