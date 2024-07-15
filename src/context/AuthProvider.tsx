713fbc3c02fe3780aeecc6d4a316572459c92d93
Abhigyan <singhdefinite12@gmail.com>
2024-07-15 17:53:17 +0530

    Auth done

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,21 +1,18 @@
-'use client'
-import { SessionProvider } from "next-auth/react"
-import { Html } from "next/document"
+'use client';
+
+import { SessionProvider } from 'next-auth/react';
+
 export default function AuthProvider({
-   children,
-  
-}:(children :React.ReactNode)) {
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
- <html lang="en">
-    <AuthProvider>
-
-    </AuthProvider>
-        <body className={inter.className}>{children}</body>
-
     <SessionProvider>
-      <children>
+      {children}
     </SessionProvider>
- </html>
-    
-  )
+  );
 }
+
+
+
